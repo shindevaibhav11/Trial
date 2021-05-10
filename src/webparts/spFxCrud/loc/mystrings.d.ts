a10e87c00ec70e6dc1ec6c5985d27844a380fac8
Shinde <vaibhav.3.shinde@atos.net>
2021-05-10 17:54:31 +0530

    first commit

---
+++ src/webparts/spFxCrud/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpFxCrudWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpFxCrudWebPartStrings' {
+  const strings: ISpFxCrudWebPartStrings;
+  export = strings;
+}
